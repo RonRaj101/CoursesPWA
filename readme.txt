3d09f6f7b20596824a5f77657270fe7a2f17bdcb
Ronit Rai <43289911+RonRaj101@users.noreply.github.com>
2023-03-23 17:30:01 +0000

    remote

---
+++ readme.txt
@@ -0,0 +1,2 @@
+Repo: https://github.com/RonRaj101/Web-Based-CW3-Individual 
+Pages: https://ronraj101.github.io/Web-Based-CW3-Individual/
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/WEB BASED CW3 INDIVIDUAL/",
   plugins: [vue()],
   resolve: {
     alias: {
